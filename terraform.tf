5f26e4a8e21ed9bc597533262561743f3f4313dd
ned1313 <ned.bellavance@gmail.com>
2023-10-09 22:16:51 -0400

    fix state

--- terraform.tf
+++ terraform.tf
@@ -10,6 +10,6 @@ terraform {
     # Details configured by GitHub Actions
     use_azuread_auth = true
     use_oidc         = true
-    key              = "dev.tfstate"
+    key              = "prod.tfstate"
   }
 }
